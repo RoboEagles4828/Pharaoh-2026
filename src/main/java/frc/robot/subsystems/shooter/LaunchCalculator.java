d7a0c0106812ab95aff8fd36242b6aaf059452e0
bdunko <bdunko@gmail.com>
2026-02-28 11:10:41 -0500

    Update shoot from anywhere tables with real data; remove ShooterParameters

--- src/main/java/frc/robot/subsystems/shooter/LaunchCalculator.java
+++ src/main/java/frc/robot/subsystems/shooter/LaunchCalculator.java
@@ -1,31 +1,11 @@
 package frc.robot.subsystems.shooter;
 
-import edu.wpi.first.apriltag.AprilTagFieldLayout;
-import edu.wpi.first.apriltag.AprilTagFields;
-import edu.wpi.first.math.filter.LinearFilter;
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
-import edu.wpi.first.math.interpolation.InterpolatingTreeMap;
-import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.DriverStation.Alliance;
-import frc.robot.Constants;
-import frc.robot.util.Util4828;
 
 public class LaunchCalculator {
     private static LaunchCalculator instance;
-
-    // private final LinearFilter launchVelocityFilter = 
-    //     LinearFilter.movingAverage((int) (ShooterConstants.SECONDS_OF_DATA_TO_AVERAGE / 0.02));
-    // private final LinearFilter launchAngleFilter = 
-    //     LinearFilter.movingAverage((int) (ShooterConstants.SECONDS_OF_DATA_TO_AVERAGE / 0.02));
-
-    // private double calculatedVelocity;
-    // private double lastHoodPosition;
-    // private double calculatedHoodPosition;
-    // private boolean isScoring;
 
     public static LaunchCalculator getInstance() {
         if (instance == null) {
@@ -36,8 +16,6 @@ public class LaunchCalculator {
 
     public record LaunchParameters(double velocityMPS, double hoodPosition) {}
 
-    private LaunchParameters latestParameters = null;
-
     // maps for scoring
     private static final InterpolatingDoubleTreeMap launchVelocityMap = ShooterConstants.SHOOT_VELOCITY_MAP;
     private static final InterpolatingDoubleTreeMap launchHoodPositionMap = ShooterConstants.SHOOT_HOOD_POSITION_MAP;
@@ -46,7 +24,6 @@ public class LaunchCalculator {
     private static final InterpolatingDoubleTreeMap passVelocityMap = ShooterConstants.PASS_VELOCITY_MAP;
 
     public LaunchParameters getParameters(Pose2d robotPose, boolean isScoring) {
-
         boolean targetTop = robotPose.getY() > ShooterConstants.CENTER_HUB.getY();
 
         Translation2d targetPos = isScoring ? ShooterConstants.CENTER_HUB : 
@@ -57,8 +34,7 @@ public class LaunchCalculator {
         double targetVelocity = isScoring ? launchVelocityMap.get(distanceToTarget) : passVelocityMap.get(distanceToTarget);
         double targetHoodPosition = isScoring ? launchHoodPositionMap.get(distanceToTarget) : ShooterConstants.HOOD_MAX_POSITION;
 
-        latestParameters = new LaunchParameters(targetVelocity, targetHoodPosition);
-        return latestParameters;
+        return new LaunchParameters(targetVelocity, targetHoodPosition);
     }
 
 }
